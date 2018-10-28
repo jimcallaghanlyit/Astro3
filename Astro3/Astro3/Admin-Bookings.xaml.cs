488a4700082d53ff3fd21c79e96daa018ee5da91
James Callaghan <L00099507@student.lyit.ie>
2018-10-28 11:01:27 +0000

    Added Admin-Bookings-Page

---
+++ Astro3/Astro3/Admin-Bookings.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Astro3
+{
+    /// <summary>
+    /// Interaction logic for Admin_Bookings.xaml
+    /// </summary>
+    public partial class Admin_Bookings : Page
+    {
+        public Admin_Bookings()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
