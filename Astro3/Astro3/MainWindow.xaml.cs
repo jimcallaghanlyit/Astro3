913e991f31ed7adca18eb84f1135d46ec7a476b4
James Callaghan <L00099507@student.lyit.ie>
2018-12-22 11:34:39 +0000

    Added-Try-Catch

--- Astro3/Astro3/MainWindow.xaml.cs
+++ Astro3/Astro3/MainWindow.xaml.cs
@@ -94,27 +94,38 @@ namespace Astro3
         private bool ValidateUserInput(string username, string password)
         {
             bool validated = true;
-            // Check username length & ensure not more 
-            // or less than specfified in DB table
-            if (username.Length == 0 || username.Length > 30)
+            try
             {
-                validated = false;
-            }
-            // Check each character in Username and 
-            // make sure no numbers being used
-            foreach (char ch in username)
-            {
-                if (ch >= '0' && ch <= '9')
+                // Check username length & ensure not more 
+                // or less than specfified in DB table
+                if (username.Length == 0 || username.Length > 30)
+                {
+                    validated = false;
+                }
+                // Check each character in Username and 
+                // make sure no numbers being used
+                foreach (char ch in username)
+                {
+                    if (ch >= '0' && ch <= '9')
+                    {
+                        validated = false;
+                    }
+                }
+                // Check password length & ensure not more 
+                // or less than specfified in DB table
+                if (password.Length == 0 || password.Length > 30)
                 {
                     validated = false;
                 }
             }
-            // Check password length & ensure not more 
-            // or less than specfified in DB table
-            if (password.Length == 0 || password.Length > 30)
+            catch (Exception)
             {
+
                 validated = false;
             }
+            //It is easier to set validated to false
+            //inside on on of the checks than it is
+            //to validate each check
             return validated;
         }
 
